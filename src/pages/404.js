4b16f3d843d7aeb844677568a7ad26389390eab7
Tom Hammer <tjhammer845@gmail.com>
2021-05-11 12:35:21 -0400

    404 2

--- src/pages/404.js
+++ src/pages/404.js
@@ -27,11 +27,11 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <ThankyouContainer>
+      <PageNotFoundContainer>
         <Parallax y={[-50, 50]} tagOuter="figure">
           <GatsbyImage
             image={image}
-            alt="Thank you from Vivaz Cantina"
+            alt="Page not found on Vivaz Cantina"
             aspectRatio={256 / 90}
             placeholder="blurred"
             loading="eager"
@@ -51,13 +51,13 @@ const NotFoundPage = () => {
             </Col>
           </Row>
         </Container>
-      </ThankyouContainer>
+      </PageNotFoundContainer>
       <InstagramFeed />
     </Layout>
   )
 }
 export default NotFoundPage
-const ThankyouContainer = styled.div`
+const PageNotFoundContainer = styled.div`
   padding-top: 7rem;
   @media ${breakpoints.sm} {
     padding-top: 6rem;
