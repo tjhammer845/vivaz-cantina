4df7f7676c3b876427543bef966e2612dab853e9
Tom Hammer <tjhammer845@gmail.com>
2021-05-13 13:08:21 -0400

    map

--- src/components/mapbox.js
+++ src/components/mapbox.js
@@ -1,41 +1,19 @@
-// import Icon from "../images/vivaz-sun-icon.png"
+import React, { useState } from "react"
+import ReactMapGl from "react-map-gl"
 
-//             <img src={Icon} className="m-0" />
-//             {this.state.selectedPlace.name}
-
-import React, { useRef, useEffect, useState } from "react"
-import mapboxgl from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
-
-const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
-
-const mapContainerStyle = {
-  width: "100%",
-  height: "120px",
+export default function Map() {
+  let [viewport, setViewport] = useState({
+    latitude: 41.308189,
+    longitude: -72.933937,
+    zoom: 14,
+    width: `100%`,
+    height: `200px`,
+  })
+  return (
+    <ReactMapGl
+      mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKEN}
+      {...viewport}
+      onViewportChange={newView => setViewport(newView)}
+    ></ReactMapGl>
+  )
 }
-
-const Map = () => {
-  const mapContainerRef = useRef(null)
-
-  const [map, setMap] = useState(null)
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      accessToken: MAPBOX_TOKEN,
-      style: "mapbox://styles/mapbox/streets-v11",
-      // Empire State Building [lng, lat]
-      center: [41.308189, -72.933937],
-      zoom: 10,
-    })
-    map.addControl(new mapboxgl.NavigationControl(), "top-right")
-
-    setMap(map)
-
-    return () => map.remove()
-  }, [])
-
-  return <div ref={mapContainerRef} style={mapContainerStyle} />
-}
-
-export default Map
