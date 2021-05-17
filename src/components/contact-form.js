cdfcf73155edfb7d897fa73c376e66dcd8d5b100
Tom Hammer <tjhammer845@gmail.com>
2021-05-17 19:08:11 -0400

    another attempt

---
+++ src/components/contact-form.js
@@ -0,0 +1,73 @@
+import React from "react"
+// import Recaptcha from "react-google-recaptcha"
+import { Button, Col, Form, Row } from "react-bootstrap"
+
+export default function ContactForm() {
+  return (
+    <Form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-recaptcha="true"
+      action="/thank-you"
+      netlify-honeypot="bot-field"
+    >
+      <Row>
+        <Col md={6}>
+          <Form.Group>
+            <Form.Label htmlFor="first-name">First Name</Form.Label>
+            <Form.Control required size="lg" type="text" name="first-name" />
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group>
+            <Form.Label htmlFor="last-name">Last Name</Form.Label>
+            <Form.Control required size="lg" type="text" name="last-name" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Form.Group>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control required size="lg" type="email" name="email" />
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group>
+            <Form.Label htmlFor="phone">Phone (Optional)</Form.Label>
+            <Form.Control size="lg" type="tel" name="phone" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12}>
+          <Form.Group>
+            <Form.Label htmlFor="message">Message</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows="3"
+              placeholder="Enter your message here."
+              name="message"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12} className="d-flex justify-content-lg-end">
+          {/* <Recaptcha
+            ref="recaptcha"
+            sitekey={process.env.GATSBY_RECAPTCHA_KEY}
+            size="normal"
+            id="recaptcha-google"
+          /> */}
+          <Button className="mr-3" type="reset" value="Eraser">
+            Clear
+          </Button>
+          <Button type="submit">Send</Button>
+        </Col>
+      </Row>
+    </Form>
+  )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,13 @@
 import * as React from "react"
+import ContactForm from "../components/contact-form"
 import ContactInfo from "../components/contact-info"
 import Layout from "../components/layout"
 import Map from "../components/mapbox"
 import SEO from "../components/seo"
 import styled from "styled-components"
-import { Container, Button, Col, Form, Row } from "react-bootstrap"
-import { Parallax } from "react-scroll-parallax"
 import { breakpoints } from "../utils/breakpoints"
+import { Container, Col, Row } from "react-bootstrap"
+import { Parallax } from "react-scroll-parallax"
 
 const contact = () => {
   return (
@@ -26,86 +27,7 @@ const contact = () => {
         <Container>
           <Row>
             <Col xs={12} sm={7} md={8}>
-              <Form
-                name="contact"
-                method="post"
-                data-netlify="true"
-                data-netlify-recaptcha="true"
-                action="/thank-you"
-                netlify-honeypot="bot-field"
-              >
-                <Row>
-                  <Col md={6}>
-                    <Form.Group>
-                      <label>
-                        Don’t fill this out if you&rsquo;re human:{" "}
-                        <input name="bot-field" />
-                      </label>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label htmlFor="first-name">First Name</Form.Label>
-                      <Form.Control
-                        required
-                        size="lg"
-                        type="text"
-                        name="first-name"
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={6}>
-                    <Form.Group>
-                      <Form.Label htmlFor="last-name">Last Name</Form.Label>
-                      <Form.Control
-                        required
-                        size="lg"
-                        type="text"
-                        name="last-name"
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={6}>
-                    <Form.Group>
-                      <Form.Label htmlFor="email">Email</Form.Label>
-                      <Form.Control
-                        required
-                        size="lg"
-                        type="email"
-                        name="email"
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={6}>
-                    <Form.Group>
-                      <Form.Label htmlFor="phone">Phone (Optional)</Form.Label>
-                      <Form.Control size="lg" type="tel" name="phone" />
-                    </Form.Group>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={12}>
-                    <Form.Group>
-                      <Form.Label htmlFor="message">Message</Form.Label>
-                      <Form.Control
-                        required
-                        as="textarea"
-                        rows="3"
-                        placeholder="Enter your message here."
-                        name="message"
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={12} className="d-flex justify-content-lg-end">
-                    <Button className="mr-3" type="reset" value="Eraser">
-                      Clear
-                    </Button>
-                    <Button type="submit">Send</Button>
-                  </Col>
-                </Row>
-              </Form>
+              <ContactForm />
             </Col>
             <Col xs={12} sm={5} md={4}>
               <ContactInfo />
