4b26fb43215a434093485d5d4df89e8564cff052
Tom Hammer <tjhammer845@gmail.com>
2021-05-21 12:36:27 -0400

    again again

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -25,7 +25,7 @@ export default function ContactForm() {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        "form-name": form.getAttribute("name"),
+        "form-name": form.getAttribute("contact"),
         "g-recaptcha-response": recaptchaValue,
         ...state,
       }),
