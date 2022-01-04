0cf656c0587e1bf4c87a1df0b4dbd0758c27ea71
Tom Hammer <tomh@laseraway.net>
2022-01-04 16:57:11 -0500

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -49,49 +49,10 @@ const about = () => {
         <Container>
           <Row>
             <Col sm={12}>
-              <h2>A Brief History of Vivaz Cantina</h2>
+              <h2>About Vivaz Cantina</h2>
               <p>
-                Nunc ulvinar sapien et ligula ullamcorper malesuada proin.
-                Sollicitudin nibh sit amet commodo nulla facilisi. Aliquam sem
-                fringilla ut morbi tincidunt augue interdum velit. Sapien nec
-                sagittis aliquam malesuada bibendum. Et tortor consequat id
-                porta nibh venenatis cras. Elit sed vulputate mi sit amet. At
-                ultrices mi tempus imperdiet. Dignissim diam quis enim lobortis
-                scelerisque fermentum dui faucibus in. A pellentesque sit amet
-                porttitor eget dolor morbi. Sollicitudin aliquam ultrices
-                sagittis orci a. Eget gravida cum sociis natoque penatibus et
-                magnis dis.
+                We're working on it. Content coming soon!
               </p>
-              <p>
-                Nunc ulvinar sapien et ligula ullamcorper malesuada proin.
-                Sollicitudin nibh sit amet commodo nulla facilisi. Aliquam sem
-                fringilla ut morbi tincidunt augue interdum velit. Sapien nec
-                sagittis aliquam malesuada bibendum. Et tortor consequat id
-                porta nibh venenatis cras. Elit sed vulputate mi sit amet. At
-                ultrices mi tempus imperdiet. Dignissim diam quis enim lobortis
-                scelerisque fermentum dui faucibus in. A pellentesque sit amet
-                porttitor eget dolor morbi. Sollicitudin aliquam ultrices
-                sagittis orci a. Eget gravida cum sociis natoque penatibus et
-                magnis dis.
-              </p>
-            </Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row className="pb-5">
-            <Col xs={12} sm={6}>
-              <User
-                username="Bernard Lanche"
-                avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
-                excerpt="I'm Maria Randall. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-              />
-            </Col>
-            <Col xs={12} sm={6}>
-              <User
-                username="David Klemen"
-                avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
-                excerpt="I'm Daniela Dewitt. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-              />
             </Col>
           </Row>
         </Container>
