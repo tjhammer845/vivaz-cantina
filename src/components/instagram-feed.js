ba589154d48a953948c2902e44e2a5335627f14a
Tom Hammer <tomh@laseraway.net>
2022-01-05 16:51:24 -0500

    changes

--- src/components/instagram-feed.js
+++ src/components/instagram-feed.js
@@ -36,7 +36,7 @@ const InstagramFeed = props => {
   const data = useStaticQuery(graphql`
     query InstagramQuery {
       allInstagramContent(
-        sort: { fields: timestamp, order: DESC }
+        sort: { fields: localImage___modifiedTime, order: DESC }
       ) {
         edges {
           node {
