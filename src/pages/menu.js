18c0563dc8d349b9474997494419ac765920bb8f
Tom Hammer <tomh@laseraway.net>
2022-08-29 19:55:33 -0400

    remove min height in mobile' on menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -52,6 +52,7 @@ const Menu = ({ data: { item } }) => (
               <p>
               Our dishes and drinks are always evolving to capture the tastes between traditional and contemporary Mexican cuisines. We find inspiration in  gourmet ingredients while staying true to our family flavors. 
               </p>
+              <a href="/pdfs/vivaz-cantina-menu.pdf" classname="block btn btn-primary mt-5">PDF Menu</a>
             </Col>
           </Row>
         </Container>
